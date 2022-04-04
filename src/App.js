7ad2657296645de3c0dee3f0b7044d01897a47a6
Howl <howl@MacBook-Air-Howl.local>
2022-04-04 15:14:32 +0600

    added announcement and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./MainRoutes";
+import Announcement from "./components/Announcement/Announcement";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Announcement />
         <MainRoutes />
       </BrowserRouter>
     </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <Navbar />
+    </div>
+  );
 };
 
 export default HomePage;
