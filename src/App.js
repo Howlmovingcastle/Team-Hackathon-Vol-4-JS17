fe1cf9c11559867bd7a0a9a6dc0d299fcaffe759
Howl <howl@MacBook-Air-Howl.local>
2022-04-07 05:12:39 +0600

    added loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./MainRoutes";
@@ -9,22 +9,44 @@ import CartContextProvider from "./contexts/cartContext";
 
 import "./index.css";
 import FavoriteContextProvider from "./contexts/favoriteContext";
+import { HashLoader } from "react-spinners";
+import { css } from "@emotion/react";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+    margin-top: 250px;
+  `;
+
   return (
     <>
-      <BrowserRouter>
-        <AuthContextProvider>
-          <ProductsContextProvider>
-            <CartContextProvider>
-              <FavoriteContextProvider>
-                <Announcement />
-                <MainRoutes />
-              </FavoriteContextProvider>
-            </CartContextProvider>
-          </ProductsContextProvider>
-        </AuthContextProvider>
-      </BrowserRouter>
+      {loading ? (
+        <HashLoader css={override} color="F4A0E8" loading={loading} size={75} />
+      ) : (
+        <BrowserRouter>
+          <AuthContextProvider>
+            <ProductsContextProvider>
+              <CartContextProvider>
+                <FavoriteContextProvider>
+                  <Announcement />
+                  <MainRoutes />
+                </FavoriteContextProvider>
+              </CartContextProvider>
+            </ProductsContextProvider>
+          </AuthContextProvider>
+        </BrowserRouter>
+      )}
     </>
   );
 };
